cf3ade7108a18d699882662bee85aa864dc69b94
Terry Koek <terrykoek@Terrys-MacBook-Pro.local>
2021-06-14 00:27:55 +0800

    update

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-$(() => {
+$(function () {
     //start game function
     let startGame;
     let endGame = false;
@@ -7,21 +7,21 @@ $(() => {
     let score = $('#score');
     let scoreCount = 1;
 
-    
+
     //creating variables for buttons
     let restartbtn = $('#restart');
     let restartdiv = $('#restartdiv');
 
-     //variable for joke and punchline function
-     let jokes;
-     let punchLine;
- 
-    
+    //variable for joke and punchline function
+    let jokes;
+    let punchLine;
+
+
     //creating basic function for lines
     let line1 = $('#firstLine');
     let line2 = $('#secondLine');
     let line3 = $('#thirdLine');
-    
+
 
     //creating basic function for cars 
     let car = $('#car');
@@ -40,7 +40,7 @@ $(() => {
     let moveDown = false;
     let moveRight = false;
     let moveLeft = false;
- 
+
 
 
     //saving some initial setup
@@ -55,15 +55,18 @@ $(() => {
 
 
     //move player car
-    
+
     //requestAnimation to call function left over a period of time
     //keydown is the action of pressing button down
+    //listening on 'document for a clik on keyboard down, a function with event will occur
     $(document).on('keydown', function (e) {    //keycode for keydown=40
         //The window.requestAnimationFrame() method tells the browser that you wish to perform an animation and requests that the browser calls a specified function to update an animation before the next repaint. 
         //The method takes a callback as an argument to be invoked before the repaint.
         if (endGame === false) {
             let key = e.keyCode;
             if (key === 37) { //keycode for left key is 37
+                //requestAnimaitonFrame tells browser i wish to perform an animation and request that browser calls specific function to update an animation before next repaint.
+                //need to call request animationframe again as its a 1shot
                 moveLeft = requestAnimationFrame(left);
             } else if (key === 39) {
                 moveRight = requestAnimationFrame(right);
@@ -75,7 +78,7 @@ $(() => {
         }
     });
 
-   
+
     //keyup is the action of releasing of button on keyboard
 
     $(document).on('keyup', function (e) {
@@ -130,7 +133,7 @@ $(() => {
     //assign startGame function to repeat this animation
     startGame = requestAnimationFrame(repeat);
 
-    
+
     //creating the movement of opposite car
     function carDown(car) {
         let currentTopCar = parseInt(car.css('top')); //returns the value of the top of the opposite car with respect to the top currently
@@ -154,67 +157,67 @@ $(() => {
     //collision function
     //2 parameters of player car and the opposite car will run in this function
     function collision($para1, $para2) {
-     //retrieve a position of an element containing the properties of top respect to the bottom of container
-    let y1 = $para1.offset().top; 
-     //Get the current computed outer height (including padding, border, and optionally margin) 
-    let h1 = $para1.outerHeight(true); 
-    //to get the complete distance of the top of the player car with respect to the bottom of the container
-     let b1 = y1 + h1;
+        //retrieve a position of an element containing the properties of top respect to the bottom of container
+        let y1 = $para1.offset().top;
+        //Get the current computed outer height (including padding, border, and optionally margin) 
+        let h1 = $para1.outerHeight(true);
+        //to get the complete distance of the top of the player car with respect to the bottom of the container
+        let b1 = y1 + h1;
 
-    //do the same for left with respect to the left of the container
-    let x1 = $para1.offset().left; 
-    let w1 = $para1.outerWidth(true); //Get the outerwidth of the div including padding and margin
-    //to get the complete distance of the left of the player car with respect to the left of the container
-    let r1 = x1 + w1;
+        //do the same for left with respect to the left of the container
+        let x1 = $para1.offset().left;
+        let w1 = $para1.outerWidth(true); //Get the outerwidth of the div including padding and margin
+        //to get the complete distance of the left of the player car with respect to the left of the container
+        let r1 = x1 + w1;
 
-    //same thing for 2nd parameter
-    let y2 = $para2.offset().top;
-    let h2 = $para2.outerHeight(true);
-    let x2 = $para2.offset().left;
-    let w2 = $para2.outerWidth(true);
-    let b2 = y2 + h2;
-    let r2 = x2 + w2;
+        //same thing for 2nd parameter
+        let y2 = $para2.offset().top;
+        let h2 = $para2.outerHeight(true);
+        let x2 = $para2.offset().left;
+        let w2 = $para2.outerWidth(true);
+        let b2 = y2 + h2;
+        let r2 = x2 + w2;
 
-    //if displacement of player car(b1) < displacement of opp car (y2) relative to bottom of container, this means the opp car is always above player car, this will return false which means no collision. same thing for the left and right and bottom
-    if (y1 > b2 ||  b1 < y2|| x1 > r2|| r1 < x2) {
-        return false;
+        //if displacement of player car(b1) < displacement of opp car (y2) relative to bottom of container, this means the opp car is always above player car, this will return false which means no collision. same thing for the left and right and bottom
+        if (y1 > b2 || b1 < y2 || x1 > r2 || r1 < x2) {
+            return false;
+        }
+
+        return true;
     }
-
-    return true;
-}
 
 
     //define repeat function
     function repeat() {
-    if (collision(car, car1) || collision(car, car2) || collision(car, car3)) { //if main car collide with car 1, 2 or 3, gamestop funciton will kick in which will stop the game
-        gameStop();
-        return;
+        if (collision(car, car1) || collision(car, car2) || collision(car, car3)) { //if main car collide with car 1, 2 or 3, gamestop funciton will kick in which will stop the game
+            gameStop();
+            return;
+        }
+
+        scoreCount++;
+
+        if (scoreCount % 50 == 0) {
+            score.text(parseInt(score.text()) + 1);
+        }
+
+        if (scoreCount % 100 == 0) { //increase speed of opposite car
+            carSpeed++;
+            lineSpeed++;
+        }
+
+        //defining car down function with parameters of car 1,2 and 3
+        carDown(car1);
+        carDown(car2);
+        carDown(car3);
+
+        //defining line down function with parameters of line 1,2 and 3
+
+        lineDown(line1);
+        lineDown(line2);
+        lineDown(line3);
+
+        startGame = requestAnimationFrame(repeat);
     }
-
-    scoreCount++;
-
-    if (scoreCount % 30 == 0) {
-        score.text(parseInt(score.text()) + 1);
-    }
-
-    if (scoreCount % 300 == 0) { //increase speed of opposite car
-        carSpeed++;
-        lineSpeed++;
-    }
-
-    //defining car down function with parameters of car 1,2 and 3
-    carDown(car1);
-    carDown(car2);
-    carDown(car3);
-
-    //defining line down function with parameters of line 1,2 and 3
-
-    lineDown(line1);
-    lineDown(line2);
-    lineDown(line3);
-
-    startGame = requestAnimationFrame(repeat);
-}
 
     //restart button is clicked, reload the page and activate gameStop function to cancel animation
     restartbtn.click(function () {
@@ -234,9 +237,9 @@ $(() => {
         fetchRandomJoke();
 
     }
-    
+
     // input joke API
-    
+
     fetchRandomJoke = () => {
         fetch('https://official-joke-api.appspot.com/random_joke', {// fetch from the API
             headers: {
